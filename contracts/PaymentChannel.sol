94d162acc9d115d24d3deadc69a97f9e386fdb3a
grzeznx2 <grzeznx2@gmail.com>
2023-02-08 19:08:05 +0100

    Add storage variables and constructor

--- contracts/PaymentChannel.sol
+++ contracts/PaymentChannel.sol
@@ -2,5 +2,13 @@
 pragma solidity ^0.8.9;
 
 contract PaymentChannel {
+    address public sender;
+    address public recipient;
+    uint256 public expiration;
 
+    constructor(address _recipient, uint256 _duration) payable {
+        sender = msg.sender;
+        recipient = _recipient;
+        expiration = block.timestamp + _duration;
+    }
 }
