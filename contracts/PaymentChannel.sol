c30f221b803fd8bb21cc88644fb53ec51e31a2a4
grzeznx2 <grzeznx2@gmail.com>
2023-02-08 19:15:32 +0100

    Add splitSignature function

--- contracts/PaymentChannel.sol
+++ contracts/PaymentChannel.sol
@@ -11,4 +11,20 @@ contract PaymentChannel {
         recipient = _recipient;
         expiration = block.timestamp + _duration;
     }
+
+    function splitSignature(bytes memory sig) internal pure returns(uint8 v, bytes32 r, bytes32 s){
+        require(sig.length == 65, "Invalid signature length");
+
+        assembly {
+            // sig - pointer of sig length
+            // sig + 32 - first 32 bytes of sig, after sig length
+            r := mload(add(sig, 32))
+            // second 32 bytes
+            s := mload(add(sig, 64))
+            // final (1) byte (first byte of the next 32 bytes)
+            v := byte(0, mload(add(sig, 96)))
+        }
+
+        // implicit return 
+    }
 }
